37555a9d87dadbff91aedef1083e260cf31c5a84
burtonjoshuatoone <burtonjoshuatoone@gmail.com>
2026-04-23 17:05:54 -0600

    Assignment done and works on the localhost

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+import BucketList from "./FindAndFix";
+import Form from "./Updates";
+import Form1 from "./RequestCounter";
+import Counter from "./Queueing";
+
+export default function interactivity() {
+  return (
+    <>
+      <BucketList />
+      <Form />
+      <Form1 />
+      <Counter />
+    </>
+  );
+}
